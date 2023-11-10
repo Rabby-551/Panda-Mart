ee08227753a11aa6c32603136d2bb984a5a364b4
Rabby <58666715+Rabby-551@users.noreply.github.com>
2023-11-10 20:33:43 +0600

    first commit

---
+++ newslettter.php
@@ -0,0 +1,41 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://www.facebook.com/hasan.rabby.16?mibextid=LQQJ4d"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://twitter.com/Rabby2819"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/rabby_rock/?igshid=MjEwN2IyYWYwYw%3D%3D"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/Rabby-551"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
